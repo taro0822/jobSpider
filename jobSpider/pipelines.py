9b7c72c3dea2d601afe95782135bb2b5e5752267
John Li <johnli@pinkfloyd.cn>
2017-08-21 20:18:41 +0800

    set data store to mongodb database

--- jobSpider/pipelines.py
+++ jobSpider/pipelines.py
@@ -6,12 +6,17 @@
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
 import json
+from pymongo import MongoClient
 class JobspiderPipeline(object):
-    def __init__(self):
-        # 将爬取到的数据存入python.json文件中
-        self.file = open('java.json','wb')
+    # def __init__(self):
+    #     将爬取到的数据存入python.json文件中
+    #     # self.file = open('java.json','wb')
+    #     pass
 
     def process_item(self, item, spider):
-        line = str(item)
-        self.file.write(line.encode('utf-8') + "\n".encode('utf-8'))
+        # line = str(item)
+        # self.file.write(line.encode('utf-8') + "\n".encode('utf-8'))
+        db = MongoClient('localhost',27017)['pySpider']
+        id = db['python'].insert_one(dict(item)).inserted_id
+        print('insert: ', id)
         return item
